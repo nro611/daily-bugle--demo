dacdbcf27390243b5c75d541a5a276b938dabe51
Nagy Roland <nagyroland.1989@hotmail.com>
2021-05-20 21:36:05 +0200

    minor fix

--- src/main/java/hu/progmasters/ujratervezes/week16/dailybugle/repository/ReaderRepository.java
+++ src/main/java/hu/progmasters/ujratervezes/week16/dailybugle/repository/ReaderRepository.java
@@ -2,7 +2,9 @@ package hu.progmasters.ujratervezes.week16.dailybugle.repository;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public class ReaderRepository {
 
     private final JdbcTemplate jdbcTemplate;
