c3bf1bc1a8fcc683a132fe72fcebb94e908e5308
Nagy Roland <nagyroland.1989@hotmail.com>
2021-05-21 00:48:55 +0200

    renamed

--- src/main/java/hu/progmasters/ujratervezes/week16/dailybugle/dto/ReaderDto.java
+++ src/main/java/hu/progmasters/ujratervezes/week16/dailybugle/dto/ReaderDto.java
@@ -7,7 +7,7 @@ import lombok.Setter;
 @NoArgsConstructor
 @Getter
 @Setter
-public class ReaderCreateUpdateData {
+public class ReaderDto {
     private String userName;
     private String email;
 }
