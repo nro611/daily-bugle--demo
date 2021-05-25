ad7d03aaaca7b05cd8bc641e479cdf82ac7f09f9
Nagy Roland <nagyroland.1989@hotmail.com>
2021-05-25 03:11:01 +0200

    removed unused import

--- src/main/java/hu/progmasters/ujratervezes/week16/dailybugle/dto/PublicistDto.java
+++ src/main/java/hu/progmasters/ujratervezes/week16/dailybugle/dto/PublicistDto.java
@@ -3,7 +3,6 @@ package hu.progmasters.ujratervezes.week16.dailybugle.dto;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import lombok.With;
 
 @NoArgsConstructor
 @Getter
