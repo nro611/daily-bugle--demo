f44a19a9a1520cfa2f0d07e11b9b4a51a7720508
rotterandras <rotterandras@gmail.com>
2021-06-08 01:04:45 +0200

    ArticleListMapper extracted ArticleRepositoryTest init - needs tests

---
+++ src/main/java/hu/progmasters/ujratervezes/week16/dailybugle/repository/ArticleListMapper.java
@@ -0,0 +1,25 @@
+package hu.progmasters.ujratervezes.week16.dailybugle.repository;
+
+import hu.progmasters.ujratervezes.week16.dailybugle.dto.ArticleListDto;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Component;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+@Component
+public class ArticleListMapper implements RowMapper<ArticleListDto> {
+   
+   @Override
+   public ArticleListDto mapRow(ResultSet resultSet, int i) throws SQLException {
+      ArticleListDto article = new ArticleListDto();
+      article.setId(resultSet.getInt("id"));
+      article.setPublicistName(resultSet.getString("name"));
+      article.setTitle(resultSet.getString("title"));
+      article.setSynopsys(resultSet.getString("synopsys"));
+      article.setAvgRating(resultSet.getDouble("avg_rating"));
+      article.setNumOfRatings(resultSet.getInt("number_of_ratings"));
+      article.setNumOfComments(resultSet.getInt("number_of_comments"));
+      return article;
+   }
+}
