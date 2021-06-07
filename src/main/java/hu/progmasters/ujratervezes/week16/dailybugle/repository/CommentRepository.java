19e557eeefb29f9ada6c9b0f77a79db7f6afb2db
rotterandras <rotterandras@gmail.com>
2021-06-07 20:39:37 +0200

    Logging works

--- src/main/java/hu/progmasters/ujratervezes/week16/dailybugle/repository/CommentRepository.java
+++ src/main/java/hu/progmasters/ujratervezes/week16/dailybugle/repository/CommentRepository.java
@@ -2,63 +2,66 @@ package hu.progmasters.ujratervezes.week16.dailybugle.repository;
 
 import hu.progmasters.ujratervezes.week16.dailybugle.domain.Comment;
 import hu.progmasters.ujratervezes.week16.dailybugle.dto.CommentDto;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
-import java.time.LocalDateTime;
-import java.util.ArrayList;
 import java.util.List;
 
 @Repository
+@Slf4j(topic = "CommentRepository")
 public class CommentRepository {
-
-    private final JdbcTemplate jdbcTemplate;
-
-    @Autowired
-    public CommentRepository(JdbcTemplate jdbcTemplate) {
-        this.jdbcTemplate = jdbcTemplate;
-    }
-
-    public boolean saveComment(CommentDto data) {
-        String sql = "INSERT INTO comment (reader_id, comment_text, article_id, created_at) " +
-                "VALUES (?, ?, ?, ?)";
-        try {
-            int rowsAffected = jdbcTemplate.update(sql,
-                    data.getReaderId(),
-                    data.getCommentText(),
-                    data.getArticleId(),
-                    data.getCreatedAt()
-            );
-            return rowsAffected == 1;
-        } catch (DataAccessException e) {
-            return false;
-        }
-    }
-    
-    public Comment getComment(int commentId) {
-        String sql = "SELECT comment.id, reader.username, comment.comment_text, comment.article_id " +
-                "FROM comment " +
-                "JOIN reader ON comment.reader_id = reader.id " +
-                "WHERE comment.id=?";
-        Comment comment;
-        try {
-            comment = jdbcTemplate.queryForObject(sql, (rs, row) -> {
-                Comment tempComment = new Comment();
-                tempComment.setId(rs.getInt("id"));
-                tempComment.setCommentAuthor(rs.getString("username"));
-                tempComment.setCommentText(rs.getString("comment_text"));
-                tempComment.setArticleId(rs.getInt("article_id"));
-                return tempComment;
-            }, commentId);
-        } catch (DataAccessException exception) {
-            comment = null;
-        }
-        return comment;
-    }
-    
-    public List<Comment> getArticleComments(int articleId) {
+   
+   private final JdbcTemplate jdbcTemplate;
+   
+   @Autowired
+   public CommentRepository(JdbcTemplate jdbcTemplate) {
+      this.jdbcTemplate = jdbcTemplate;
+   }
+   
+   public boolean saveComment(CommentDto data) {
+      String sql = "INSERT INTO comment (reader_id, comment_text, article_id, created_at) " +
+              "VALUES (?, ?, ?, ?)";
+      try {
+         int rowsAffected = jdbcTemplate.update(sql,
+                 data.getReaderId(),
+                 data.getCommentText(),
+                 data.getArticleId(),
+                 data.getCreatedAt()
+         );
+         return rowsAffected == 1;
+      }
+      catch (DataAccessException e) {
+         logger.error(e.getMessage());
+         return false;
+      }
+   }
+   
+   public Comment getComment(int commentId) {
+      String sql = "SELECT comment.id, reader.username, comment.comment_text, comment.article_id " +
+              "FROM comment " +
+              "JOIN reader ON comment.reader_id = reader.id " +
+              "WHERE comment.id=?";
+      Comment comment;
+      try {
+         comment = jdbcTemplate.queryForObject(sql, (rs, row) -> {
+            Comment tempComment = new Comment();
+            tempComment.setId(rs.getInt("id"));
+            tempComment.setCommentAuthor(rs.getString("username"));
+            tempComment.setCommentText(rs.getString("comment_text"));
+            tempComment.setArticleId(rs.getInt("article_id"));
+            return tempComment;
+         }, commentId);
+      }
+      catch (DataAccessException exception) {
+         comment = null;
+      }
+      return comment;
+   }
+   
+   public List<Comment> getArticleComments(int articleId) {
 /*
         String sql = "SELECT comment.id, reader.username, comment.comment_text, comment.article_id " +
                 "FROM article " +
@@ -66,23 +69,24 @@ public class CommentRepository {
                 "JOIN reader ON comment.reader_id = reader.id " +
                 "WHERE article.id=?";
 */
-        String sql = "SELECT comment.id, reader.username, comment.comment_text, comment.article_id " +
-                "FROM comment " +
-                "JOIN reader ON comment.reader_id = reader.id " +
-                "WHERE comment.article_id=?";
-        List<Comment> comments;
-        try {
-            comments = jdbcTemplate.query(sql, (rs, row) -> {
-                Comment comment = new Comment();
-                comment.setId(rs.getInt("id"));
-                comment.setCommentAuthor(rs.getString("username"));
-                comment.setCommentText(rs.getString("comment_text"));
-                comment.setArticleId(rs.getInt("article_id"));
-                return comment;
-            }, articleId);
-        } catch (DataAccessException exception) {
-            comments = null;
-        }
-        return comments;
-    }
+      String sql = "SELECT comment.id, reader.username, comment.comment_text, comment.article_id " +
+              "FROM comment " +
+              "JOIN reader ON comment.reader_id = reader.id " +
+              "WHERE comment.article_id=?";
+      List<Comment> comments;
+      try {
+         comments = jdbcTemplate.query(sql, (rs, row) -> {
+            Comment comment = new Comment();
+            comment.setId(rs.getInt("id"));
+            comment.setCommentAuthor(rs.getString("username"));
+            comment.setCommentText(rs.getString("comment_text"));
+            comment.setArticleId(rs.getInt("article_id"));
+            return comment;
+         }, articleId);
+      }
+      catch (DataAccessException exception) {
+         comments = null;
+      }
+      return comments;
+   }
 }
