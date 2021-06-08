0da9ccbff6fca26ed9505f846def8c07b542a29d
Nagy Roland <nagyroland.1989@hotmail.com>
2021-06-08 13:21:17 +0200

    added ArticleQuery to store sql queries from ArticleRepository removeArticleKeywords() and getKeywordIds() need to move to become a static method in query class

--- src/main/java/hu/progmasters/ujratervezes/week16/dailybugle/repository/ArticleQuery.java
+++ src/main/java/hu/progmasters/ujratervezes/week16/dailybugle/repository/ArticleQuery.java
@@ -1,13 +1,8 @@
 package hu.progmasters.ujratervezes.week16.dailybugle.repository;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-
-@AllArgsConstructor
-@Getter
-public enum ArticleQuery {
+public class ArticleQuery {
     // Get every article in a list with average rating (2 decimals) and number of ratings and comments
-    GET_ALL_ARTICLE("SELECT article.id, " +
+    public static final String GET_ARTICLES = "SELECT article.id, " +
             "article.title, " +
             "article.synopsys, " +
             "publicist.name, " +
@@ -24,18 +19,18 @@ public enum ArticleQuery {
             "  GROUP BY article_id " +
             ") c ON article.id = c.article_id " +
             "WHERE article.status = 1 AND (article.deployed_at <= CURRENT_TIMESTAMP OR article.deployed_at IS NULL)" +
-            "GROUP BY article.id;"),
+            "GROUP BY article.id;";
     // Get article by id
-    GET_ARTICLE_ID("SELECT article.id, publicist_id, title, synopsys, text, publicist.name, " +
+    public static final String GET_ARTICLE = "SELECT article.id, publicist_id, title, synopsys, text, publicist.name, " +
             "IFNULL(CAST(AVG(rating.article_rating) AS DECIMAL(3,2)),0) AS avg_rating, " +
             "IFNULL(COUNT(rating.article_rating),0) AS number_of_ratings " +
             "FROM article " +
             "JOIN publicist ON publicist.id = publicist_id " +
             "LEFT JOIN rating ON rating.article_id = article.id " +
             "WHERE article.id = ? AND article.status = 1 " +
-            "GROUP BY article.id"),
+            "GROUP BY article.id";
     // Get the 10 latest articles
-    GET_FRESH_ARTICLE("SELECT article.id, article.title, article.synopsys, publicist.name, " +
+    public static final String GET_FRESH_ARTICLES = "SELECT article.id, article.title, article.synopsys, publicist.name, " +
             "IFNULL(CAST(AVG(rating.article_rating) AS DECIMAL(3,2)),0) AS avg_rating, " +
             "IFNULL(COUNT(rating.article_rating),0) AS number_of_ratings, " +
             "IFNULL(c.CommentCount,0) AS number_of_comments " +
@@ -51,9 +46,9 @@ public enum ArticleQuery {
             "WHERE article.status = 1 AND (article.deployed_at <= CURRENT_TIMESTAMP OR article.deployed_at IS NULL)" +
             "GROUP BY article.id " +
             "ORDER BY article.created_at DESC " +
-            "LIMIT 10"),
+            "LIMIT 10";
     // Get the 10 best rated articles
-    GET_TOP_ARTICLE("SELECT article.id, article.title, article.synopsys, publicist.name, " +
+    public static final String GET_TOP_ARTICLES = "SELECT article.id, article.title, article.synopsys, publicist.name, " +
             "IFNULL(CAST(AVG(rating.article_rating) AS DECIMAL(3,2)),0) AS avg_rating, " +
             "IFNULL(COUNT(rating.article_rating),0) AS number_of_ratings, " +
             "IFNULL(c.CommentCount,0) AS number_of_comments " +
@@ -69,9 +64,9 @@ public enum ArticleQuery {
             "WHERE article.status = 1 AND (article.deployed_at <= CURRENT_TIMESTAMP OR article.deployed_at IS NULL)" +
             "GROUP BY article.id " +
             "ORDER BY avg_rating DESC " +
-            "LIMIT 10"),
+            "LIMIT 10";
     // Get the 10 best rated articles that are 3 days old at most
-    GET_TOP_FRESH_ARTICLE("SELECT article.id, article.title, article.synopsys, publicist.name, " +
+    public static final String GET_TOP_FRESH_ARTICLES = "SELECT article.id, article.title, article.synopsys, publicist.name, " +
             "IFNULL(CAST(AVG(rating.article_rating) AS DECIMAL(3,2)),0) AS avg_rating, " +
             "IFNULL(COUNT(rating.article_rating),0) AS number_of_ratings," +
             "IFNULL(c.CommentCount,0) AS number_of_comments " +
@@ -88,45 +83,53 @@ public enum ArticleQuery {
             "article.status = 1 AND (article.deployed_at <= CURRENT_TIMESTAMP OR article.deployed_at IS NULL)" +
             "GROUP BY article.id " +
             "ORDER BY avg_rating DESC " +
-            "LIMIT 10"),
+            "LIMIT 10";
     // Update article
-    UPDATE_ARTICLE("UPDATE article SET " +
+    public static final String UPDATE_ARTICLE = "UPDATE article SET " +
             "title = ?, " +
             "synopsys = ?, " +
             "text = ?, " +
             "deployed_at = ?, " +
             "modified_at = ? " +
-            "WHERE id = ?"),
+            "WHERE id = ?";
     // Update article status to 0 <inactive>
-    DELETE_ARTICLE("UPDATE article SET " +
+    public static final String DELETE_ARTICLE = "UPDATE article SET " +
             "status = 0, " +
             "modified_at = ? " +
-            "WHERE id = ?"),
-    // Import article into db
-    SAVE_ARTICLE("INSERT INTO article (publicist_id, title, synopsys, text, created_at, deployed_at) " +
-            "VALUES (?, ?, ?, ?, ?, ?)"),
-    GET_RATING_USER_ARTICLE("SELECT reader_id, article_rating " +
+            "WHERE id = ?";
+    public static final String SAVE_ARTICLE = "INSERT INTO article (publicist_id, title, synopsys, text, created_at, deployed_at) " +
+            "VALUES (?, ?, ?, ?, ?, ?)";
+    public static final String GET_RATING_USER_ARTICLE = "SELECT reader_id, article_rating " +
             "FROM rating " +
-            "WHERE reader_id = ? AND article_id = ?"),
-    SAVE_RATING("INSERT INTO rating (reader_id, article_id, article_rating, created_at) " +
-            "VALUES (?, ?, ?, ?)"),
-    UPDATE_RATING("UPDATE rating SET " +
+            "WHERE reader_id = ? AND article_id = ?";
+    public static final String SAVE_RATING = "INSERT INTO rating (reader_id, article_id, article_rating, created_at) " +
+            "VALUES (?, ?, ?, ?)";
+    public static final String UPDATE_RATING = "UPDATE rating SET " +
             "article_rating = ?, " +
             "modified_at = ? " +
-            "WHERE reader_id = ? AND article_id = ?"),
-    GET_COMMENTS_FOR_ARTICLE_ID("SELECT r.username, c.comment_text, c.created_at " +
+            "WHERE reader_id = ? AND article_id = ?";
+    public static final String GET_COMMENTS_FOR_ARTICLE_ID = "SELECT r.username, c.comment_text, c.created_at " +
             "FROM comment c " +
             "JOIN reader r ON r.id = c.reader_id " +
             "WHERE c.article_id = ? " +
-            "ORDER BY c.created_at DESC"),
-    GET_KEYWORDS_FOR_ARTICE_ID("SELECT k.keyword_name FROM keyword k " +
+            "ORDER BY c.created_at DESC";
+    public static final String GET_KEYWORDS_FOR_ARTICE_ID = "SELECT k.keyword_name FROM keyword k " +
             "JOIN article_keyword ak ON ak.keyword_id = k.id " +
             "WHERE ak.article_id = ? " +
-            "ORDER BY k.keyword_name ASC"),
-    DELETE_KEYWORDS_ARTICLEID("DELETE keyword FROM keyword " +
+            "ORDER BY k.keyword_name ASC";
+    public static final String DELETE_KEYWORDS_ARTICLEID = "DELETE keyword FROM keyword " +
             "JOIN article_keyword ak ON ak.keyword_id = keyword.id " +
-            "WHERE article_id = ?");
+            "WHERE article_id = ?";
+    public static final String GET_KEYWORDS = "SELECT keyword_name FROM keyword";
+    public static final String SAVE_KEYWORD = "INSERT INTO keyword (keyword_name) VALUES (?)";
+    public static final String GET_ARTICLE_ID = "SELECT id FROM article WHERE title = ?";
+    public static final String SAVE_ARTICLE_KEYWORD = "INSERT INTO article_keyword (article_id, keyword_id) " +
+            "VALUES (?,?)";
+    public static final String REMOVE_KEYWORD = "DELETE FROM keyword WHERE id = ?";
+    public static final String CONTAINS_KEYWORD = "SELECT article_id FROM article_keyword WHERE keyword_id = ? LIMIT 1";
 
-    private final String sqlQuery;
+    private ArticleQuery() {
+    }
+
 
 }
