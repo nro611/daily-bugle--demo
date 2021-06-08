0c870c6ccf160027d44b0ebb1d9a86740a79d38e
Nagy Roland <nagyroland.1989@hotmail.com>
2021-06-08 23:45:36 +0200

    updated testNames in service tests

--- src/test/java/hu/progmasters/ujratervezes/week16/dailybugle/service/ArticleServiceTest.java
+++ src/test/java/hu/progmasters/ujratervezes/week16/dailybugle/service/ArticleServiceTest.java
@@ -1,7 +1,6 @@
 package hu.progmasters.ujratervezes.week16.dailybugle.service;
 
 import hu.progmasters.ujratervezes.week16.dailybugle.domain.Article;
-import hu.progmasters.ujratervezes.week16.dailybugle.dto.ArticleDto;
 import hu.progmasters.ujratervezes.week16.dailybugle.dto.ArticleListDto;
 import hu.progmasters.ujratervezes.week16.dailybugle.dto.ArticleRatingDto;
 import hu.progmasters.ujratervezes.week16.dailybugle.repository.ArticleRepository;
@@ -16,9 +15,9 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import java.time.Clock;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.*;
-import static org.mockito.Mockito.verifyNoMoreInteractions;
 
 @ExtendWith(MockitoExtension.class)
 class ArticleServiceTest {
@@ -43,78 +42,78 @@ class ArticleServiceTest {
       dto.setNumOfComments(3);
       return dto;
    }
-   
+
    @Test
    @DisplayName("Get articles to list")
-   void getArticles() {
+   void TestGetArticles() {
       when(repositoryMock.getArticles()).thenReturn(List.of(getArticleListDto()));
-      
+
       List<ArticleListDto> articleDtos = articleService.getArticles();
-      
+
       assertEquals(1, articleDtos.size());
       assertEquals("title", articleDtos.get(0).getTitle());
-   
+
       verify(repositoryMock, times(1)).getArticles();
       verifyNoMoreInteractions(repositoryMock);
    
    }
-   
+
    @Test
    @DisplayName("Get fresh articles to list")
-   void getFreshArticles() {
+   void TestGetFreshArticles() {
       when(repositoryMock.getFreshArticles()).thenReturn(List.of(getArticleListDto()));
-   
+
       List<ArticleListDto> articleDtos = articleService.getFreshArticles();
-   
+
       assertEquals(1, articleDtos.size());
       assertEquals("title", articleDtos.get(0).getTitle());
-   
+
       verify(repositoryMock, times(1)).getFreshArticles();
       verifyNoMoreInteractions(repositoryMock);
    
    }
-   
+
    @Test
    @DisplayName("Get top articles")
-   void getTopArticles() {
+   void TestGetTopArticles() {
       when(repositoryMock.getTopArticles()).thenReturn(List.of(getArticleListDto()));
-   
+
       List<ArticleListDto> articleDtos = articleService.getTopArticles();
-   
+
       assertEquals(1, articleDtos.size());
       assertEquals("title", articleDtos.get(0).getTitle());
-   
+
       verify(repositoryMock, times(1)).getTopArticles();
       verifyNoMoreInteractions(repositoryMock);
    
    }
-   
+
    @Test
    @DisplayName("Get top fresh articles")
-   void getTopFreshArticles() {
+   void TestGetTopFreshArticles() {
       when(repositoryMock.getTopFreshArticles()).thenReturn(List.of(getArticleListDto()));
-   
+
       List<ArticleListDto> articleDtos = articleService.getTopFreshArticles();
-   
+
       assertEquals(1, articleDtos.size());
       assertEquals("title", articleDtos.get(0).getTitle());
-   
+
       verify(repositoryMock, times(1)).getTopFreshArticles();
       verifyNoMoreInteractions(repositoryMock);
    
    }
-   
+
    @Test
    @DisplayName("Get article by ID")
-   void getArticle() {
+   void TestGetArticle() {
       Article articleToReturn = new Article();
       articleToReturn.setId(3);
       articleToReturn.setTitle("title");
-      
+
       when(repositoryMock.getArticle(Mockito.anyInt())).thenReturn(articleToReturn);
-   
+
       Article article = articleService.getArticle(3);
-   
+
       assertEquals(3, article.getId());
       assertEquals("title", article.getTitle());
    
@@ -122,32 +121,32 @@ class ArticleServiceTest {
       verifyNoMoreInteractions(repositoryMock);
    
    }
-   
+
    @Test
    @DisplayName("Save rating with valid data successful")
-   void saveRating_validRating_successful() {
+   void TestSaveRating_validRating_successful() {
       when(repositoryMock.getRatingWithUserAndArticle(Mockito.anyInt(), Mockito.anyInt())).thenReturn(null);
       when(repositoryMock.saveRating(anyInt(), anyInt(), anyInt(), any())).thenReturn(true);
-   
+
       ArticleRatingDto rating = new ArticleRatingDto();
       rating.setRating(4);
       assertTrue(articleService.saveRating(rating, 2));
-   
+
       verify(repositoryMock, times(1)).saveRating(anyInt(), anyInt(), anyInt(), any());
       verifyNoMoreInteractions(repositoryMock);
    
    
    }
-   
+
    @Test
    @DisplayName("Delete article successful")
-   void deleteArticle_successful() {
+   void TestDeleteArticle_successful() {
       when(repositoryMock.deleteArticle(anyInt(), any())).thenReturn(true);
-      
+
       assertTrue(articleService.deleteArticle(2));
-   
+
       verify(repositoryMock, times(1)).deleteArticle(Mockito.anyInt(), Mockito.any());
       verifyNoMoreInteractions(repositoryMock);
-   
+
    }
 }
