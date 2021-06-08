7641e9101d92407e1a2e43ac4c6010e708d9432c
Nagy Roland <nagyroland.1989@hotmail.com>
2021-06-08 15:57:01 +0200

    minor fix

--- src/main/java/hu/progmasters/ujratervezes/week16/dailybugle/repository/CommentQuery.java
+++ src/main/java/hu/progmasters/ujratervezes/week16/dailybugle/repository/CommentQuery.java
@@ -7,10 +7,11 @@ public class CommentQuery {
             "FROM comment " +
             "JOIN reader ON comment.reader_id = reader.id " +
             "WHERE comment.id=?";
-    public static final String GET_ARTICLE_COMMENTS = "SELECT comment.id, reader.username, comment.comment_text, comment.article_id " +
-            "FROM comment " +
-            "JOIN reader ON comment.reader_id = reader.id " +
-            "WHERE comment.article_id=?";
+    public static final String GET_ARTICLE_COMMENTS =
+            "SELECT comment.id, reader.username, comment.comment_text, comment.article_id " +
+                    "FROM comment " +
+                    "JOIN reader ON comment.reader_id = reader.id " +
+                    "WHERE comment.article_id=?";
 
     private CommentQuery() {
     }
